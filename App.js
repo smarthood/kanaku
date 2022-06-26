9ad722a1334de08e4ff73a06783bdc40d70e8130
smarthood <renolddickson18@gmail.com>
2022-06-26 10:30:36 +0530

    finished

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Container} from './appStyles'
+import Home from './Home';
+import  AsyncStorage  from '@react-native-async-storage/async-storage';
+import { useState,useEffect } from 'react';
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ready,setReady] = useState(false);
+  const initialstate = [];
+const [todo,setTodo]= useState(initialstate);
+
+  const LoadTodo = async () => {
+    try {
+    await  AsyncStorage.getItem("storedTodo").then(data=>{
+    if(data !== null){
+      setTodo(JSON.parse(data))
+    }
+  })
+  }catch{(error)=>console.log(error)}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+if(ready){
+  return(
+    <AppLoading 
+    startAsync={LoadTodo}
+    onFinish={()=>setReady(true)}
+    onError={console.log(error)}
+    />
+  )
+}
+useEffect(() => {
+  LoadTodo();
+}, []);
+  return (
+            <Container>
+              <Home todo={todo} setTodo={setTodo}/>
+              <StatusBar style="dark" />
+            </Container>
+  );
+}
